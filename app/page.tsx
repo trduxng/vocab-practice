2286ed56b4a610174e42cfa7bbb22f1e30c128ee
PhucNguyen45 <101778920+PhucNguyen45@users.noreply.github.com>
2026-04-08 15:07:23 +0700

    landing page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,36 @@
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 import { Menu } from "lucide-react";
 import { Search } from "lucide-react";
 import { Shuffle } from "lucide-react";
 import { Volume2 } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="m-0 p-0">
       <header className="bg-blue-400 flex justify-between">
-        <div className="logo">
-          <a href="">MemorizeMe</a>
+        <div>
+          <div className="logo">
+            <a href="">MemorizeMe</a>
+          </div>
+          <div className="user">
+            <Button>Log in</Button>
+            <Button>Sign up</Button>
+            <Button>
+              <Menu />
+            </Button>
+          </div>
         </div>
-        <div className="user">
-          <Button>Log in</Button>
-          <Button>Sign up</Button>
-          <Button>
-            <Menu />
-          </Button>
-        </div>
+        <nav></nav>
       </header>
       <main>
         <div className="search">
           <h1>Look up a word, learn it forever</h1>
           <div className="search-bar">
-            <input
-              type="text"
-              placeholder="Search words or vocabulary lists from books, exams or textbooks"
-            />
+            <Input placeholder="Search words or vocabulary lists from books, exams or textbooks"></Input>
             <Button>
               <Search />
             </Button>
@@ -55,7 +58,44 @@ export default function Home() {
           </div>
         </div>
         <div className="common-confused">
-          
+          <div className="header">
+            <a href="">Commonly Confused Words</a>
+            <p>A comprehensive guide to correct word choice</p>
+          </div>
+          <div className="confused-content flex">
+            <Card className="border-2 shadow ml-2 mr-3">
+              <h2>confused words....</h2>
+              <p>meanings...</p>
+              <Button>
+                read more
+                <ChevronRight />
+              </Button>
+            </Card>
+            <Card className="border-2 shadow ml-2 mr-3">
+              <h2>confused words....</h2>
+              <p>meanings...</p>
+              <Button>
+                read more
+                <ChevronRight />
+              </Button>
+            </Card>
+            <Card className="border-2 shadow ml-2 mr-3">
+              <h2>confused words....</h2>
+              <p>meanings...</p>
+              <Button>
+                read more
+                <ChevronRight />
+              </Button>
+            </Card>
+            <Card className="border-2 shadow ml-2 mr-3">
+              <h2>confused words....</h2>
+              <p>meanings...</p>
+              <Button>
+                read more
+                <ChevronRight />
+              </Button>
+            </Card>
+          </div>
         </div>
       </main>
     </div>
